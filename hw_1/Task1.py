555298081006f8a1e43f17d2a9402c614b524879
Hleb_Iuko <hiuko.contractor@libertyglobal.com>
2023-08-03 12:48:35 +0200

    Hometast 1 with fixes after comments

--- hw_1/Task1.py
+++ hw_1/Task1.py
@@ -1,17 +1,14 @@
 import random
 
 # Create a list of 100 random numbers from 0 to 1000
-my_list = [random.randint(0, 1000) for _ in range(100)]
+lst = [random.randint(0, 1000) for _ in range(100)]
 
 # Sort the list from min to max (without using sort())
-my_sorted_list = []
-for x in range(len(my_list)):
-    min_value = my_list[0]
-    for y in range(len(my_list) - 1):
-        if my_list[y + 1] < min_value:
-            min_value = my_list[y + 1]
-    my_sorted_list.append(min_value)
-    my_list.remove(min_value)
+n = len(lst)
+for i in range(n - 1):
+    for j in range(n - i - 1):
+        if lst[j] > lst[j + 1]:
+            lst[j], lst[j + 1] = lst[j + 1], lst[j]
 
 # Calculate average for even and odd numbers
 average_even = 0
@@ -19,12 +16,12 @@ average_odd = 0
 even_counter = 0
 odd_counter = 0
 
-for x in range(len(my_sorted_list)):
-    if x % 2 == 0:
-        average_odd += my_sorted_list[x]
+for i, item in enumerate(lst):
+    if i % 2 == 0:
+        average_odd += lst[i]
         odd_counter += 1
     else:
-        average_even += my_sorted_list[x]
+        average_even += lst[i]
         even_counter += 1
 
 average_even /= even_counter
