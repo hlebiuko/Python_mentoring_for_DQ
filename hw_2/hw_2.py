d723a8b6068c0f98604880320fa079e98fa6b854
Hleb_Iuko <hiuko.contractor@libertyglobal.com>
2023-08-11 23:45:59 +0200

    hw 2 after comments 3

--- hw_2/hw_2.py
+++ hw_2/hw_2.py
@@ -1,12 +1,13 @@
-# Task 2 Part 1
-# Write a code, which will:
-# 1. create a list of random number of dicts (from 2 to 10)
-# dict's random numbers of keys should be letter,
-# dict's values should be a number (0-100),
-# example: [{'a': 5, 'b': 7, 'g': 11}, {'a': 3, 'c': 35, 'g': 42}]
+'''Task 2 Part 1
+Write a code, which will:
+1. create a list of random number of dicts (from 2 to 10)
+dict's random numbers of keys should be letter,
+dict's values should be a number (0-100),
+example: [{'a': 5, 'b': 7, 'g': 11}, {'a': 3, 'c': 35, 'g': 42}]'''
 
 import random
 import string
+
 
 list_of_dicts = []
 number_of_dicts = random.randint(3, 10)
@@ -18,8 +19,6 @@ def dict_sort(my_dict: dict) -> dict:  # function that gets dict as argument and
     return {key: my_dict[key] for key in keys}  # returning dict with sorted keys
 
 
-# 'while' loop was selected because of by using 'for' loop may cause loss duplicates of keys f.e. if generated value for
-# amount of keys was 26, and we will have duplicates in keys value (2 times 'a' char) - we will have fewer keys in total
 for i in range(number_of_dicts):
     temp_dict = {}  # initialization temp dict variable to collect generated values
     number_of_pairs_in_dict = random.randint(3, 26)  # initialization and generating value for number of pairs in dict
@@ -31,14 +30,23 @@ for i in range(number_of_dicts):
     temp_dict = dict_sort(temp_dict)  # Sorting generated dict by function dict_sort
     list_of_dicts.append(temp_dict)  # Adding generated dict to the list of dicts
 
-# Task 2 Part 2
-# 2. get previously generated list of dicts and create one common dict:
-# if dicts have same key, we will take max value, and rename key with dict number with max value
-# if key is only in one dict - take it as is,
-# example:{'a_1': 5, 'b': 7, 'c': 35, 'g_2': 42}
-# Each line of code should be commented with description.
+''' Task 2 Part 2
+ 2. get previously generated list of dicts and create one common dict:
+ if dicts have same key, we will take max value, and rename key with dict number with max value
+ if key is only in one dict - take it as is,
+ example:{'a_1': 5, 'b': 7, 'c': 35, 'g_2': 42}
+ Each line of code should be commented with description.'''
+
 new_dict = {}  # Initialization new dict variable
 dict_for_changed_elements = {}  # Initialization dict variable for changed elements (key = key, value = dict number)
+dict_with_key_count = {}
+
+for dict_num, current_dict in enumerate(list_of_dicts, start=1):
+    for key in current_dict:
+        if key in dict_with_key_count:
+            dict_with_key_count[key] += 1
+        else:
+            dict_with_key_count[key] = 1
 
 for dict_num, current_dict in enumerate(list_of_dicts, start=1):        # Loop for detecting duplicates in keys
     for key in current_dict:            # loop for filling in new_dict with all values
@@ -46,14 +54,20 @@ for dict_num, current_dict in enumerate(list_of_dicts, start=1):        # Loop f
             if current_dict[key] > new_dict[key]:
                 new_dict[key] = current_dict[key]
                 dict_for_changed_elements[key] = dict_num
+            # else:
+                # dict_for_changed_elements.pop(key)
         else:
             new_dict[key] = current_dict[key]
-            dict_for_changed_elements[key] = dict_num
+            if dict_with_key_count[key] != 1:
+                dict_for_changed_elements[key] = dict_num
 
 new_final_dict = {}  # dict variable for items with proper key names
 
 for key in new_dict:        # loop for creating dict with proper keys
-    new_final_dict[key + "_" + str(dict_for_changed_elements[key])] = new_dict[key]
+    if key in dict_for_changed_elements:
+        new_final_dict[f"{key}_{dict_for_changed_elements[key]}"] = new_dict[key]
+    else:
+        new_final_dict[key] = new_dict[key]
 
 new_final_dict = dict_sort(new_final_dict)  # sorting final dict
 print(new_final_dict)  # print final result - can be deleted
